3770543871674ec86f89e935a4ffc547593c257c
Кристина <dragonfartune@gmail.com>
2025-11-11 14:44:13 +0300

    refactor: общая логика форматора вынесена в абстрактный класс
    (cherry picked from commit fbab5ce5417614805309494b6281db24d0569f05)

--- src/main/java/com/example/calendar/service/format/CsvCalendarFormatter.java
+++ src/main/java/com/example/calendar/service/format/CsvCalendarFormatter.java
@@ -1,49 +1,16 @@
 package com.example.calendar.service.format;
 
 import com.example.calendar.model.Calendar;
-import com.example.calendar.model.Month;
 
-public class CsvCalendarFormatter implements CalendarFormatter {
-    private static final String[] DAYS = {"Пн","Вт","Ср","Чт","Пт","Сб","Вс"};
+public class CsvCalendarFormatter extends BaseCalendarFormatter {
 
     @Override
-    public String format(Calendar calendar) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Месяц; ;").append(String.join(";", DAYS)).append("\n");
+    protected String separator() {
+        return ";";
+    }
 
-        int startDay = calendar.getYear().getFirstDayOfYear();
-
-        for (Month month : calendar.getMonths()) {
-            sb.append(month.name()).append(";;");
-
-            int dayOfWeek = startDay;
-
-            sb.append(";".repeat(Math.max(0, dayOfWeek)));
-
-            for (int d = 1; d <= month.daysCount(); d++) {
-                sb.append(d);
-
-                if (dayOfWeek < 6) {
-                    sb.append(";");
-                }
-
-                dayOfWeek = (dayOfWeek + 1) % 7;
-
-                if (dayOfWeek == 0 && d != month.daysCount()) {
-                    sb.append("\n;;");
-                }
-            }
-
-            if (dayOfWeek != 0) {
-                for (int i = dayOfWeek; i < 7; i++) {
-                    sb.append(";");
-                }
-            }
-
-            sb.append("\n\n");
-            startDay = dayOfWeek;
-        }
-
-        return sb.toString();
+    @Override
+    protected String header(Calendar calendar) {
+        return "Месяц;" + String.join(";", DAYS) + "\n";
     }
 }
